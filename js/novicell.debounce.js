55a950fa362c7aca45ce40ebb2113caea2dcd392
Juozas Rastenis <jra@novicell.dk>
2018-12-12 15:25:07 +0100

    Export / Import made possible

--- js/novicell.debounce.js
+++ js/novicell.debounce.js
@@ -1,23 +1,24 @@
 /**
-* @name Novicell Debounce
-* @author Danni Larsen
-* @argument func, wait, immediate
-* @description Simple function to debounce scroll and resize event
-*/
+ * @name Novicell Debounce
+ * @author Danni Larsen
+ * @argument func, wait, immediate
+ * @description Simple function to debounce scroll and resize event
+ */
 
-var novicell = novicell || {};
-
-novicell.debounce = novicell.debounce || function (func, wait, immediate) {
-	var timeout;
-	return function() {
-		var context = this, args = arguments;
-		var later = function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
+const debounce = function (func, wait, immediate) {
+    var timeout;
+    return function () {
+        var context = this,
+            args = arguments;
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
 };
+
+export default debounce;
